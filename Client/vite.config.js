14a74c7e649ae1250c5ceb631f8c25e164093b04
ReemAbutalib <reem.abutalib@gmail.com>
2025-08-18 15:49:31 +0100

    Added _redirects

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
@@ -12,6 +17,14 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
+  publicDir: 'public', // Make sure publicDir is set
   test: {
     environment: 'jsdom',
     globals: true,
